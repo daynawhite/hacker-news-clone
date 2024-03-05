736aeb54e15b094ade8ff47cd9417e4e4d53a741
daynawhite <dayna26.2@gmail.com>
2024-03-04 23:52:54 -0600

    changes made to styling; drop-down menus added

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
         )
   }
 
+
   // Here is what goes on in the GUI
   return (
     <div className="App">
@@ -40,16 +41,46 @@ function App() {
             <p>Search</p>
             <p>Hacker News</p>
           </div>
+
           <div className="search-container">
             <form >
-              <input className="searchBox" onChange={fetchResults} name="search" placeholder="Search stories by title, url, or author"/>
+              <input className="searchBox" onChange={fetchResults} name="search" placeholder="Q  Search stories by title, url, or author"/>
               {/* Possibly add some functionality in order to clear the form, etc */}
             </form>
           </div>
           <img className="gear-icon" src="https://cdn-icons-png.freepik.com/256/2040/2040504.png"/>
         </header>
+        <div className="filter-menus">
+          <form action="/action_page.php">
+            <label for="Stories"> Search </label>
+            <select id="stories" name="stories">
+              <option value="All">All</option>
+              <option value="Stories">Stories</option>
+              <option value="Comments">Comments</option>
+              <option value="Ask HN">Ask HN</option>
+              <option value="Show HN">Show HN</option>
+              <option value="Launch HN">Launch HN</option>
+              <option value="Jobs">Jobs</option>
+              <option value="Polls">Polls</option>
+            </select>
+            <label for="by"> by </label>
+            <select id="by" name="by">
+              <option value="Popularity">Popularity</option>
+              <option value="Date">Date</option>
+            </select>
+            <label for="for"> for </label>
+            <select id="for" name="for">
+              <option value="All time">All time</option>
+              <option value="Last 24h">Last 24h</option>
+              <option value="Past Week">Past Week</option>
+              <option value="Past Month">Past Month</option>
+              <option value="Past Year">Past Year</option>
+              <option value="Custom Range">Custom Range</option>
+            </select>
+          </form>
+        </div>
 
-        <ul>
+        <ul className="listing">
           {results.map((article, idx) => 
             <li  key={idx}>
                 <a className="articles" href={article.url}>
